c2b9c8e52a78b6374fcfe2cd5592578e54cf1558
Mathis Lauriol-Torcq <mathis.lauriol-torcq@etu.univ-amu.fr>
2025-01-28 14:12:17 +0100

    Ceci est mon 1er prog

---
+++ HellWorld.java
@@ -0,0 +1,7 @@
+package HellWorld;
+
+public class HellWorld {
+	public static void main(String[] args) {  // Program entry point
+	      System.out.println("hello world");    // Print text message
+	   }
+}
